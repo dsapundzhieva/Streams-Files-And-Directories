d42e02c6bf57127e49c41238b2a6ab245ca315eb
Desislava Sapundzhieva <desislava.sapundzhieva@gmail.com>
2022-06-02 23:46:40 +0300

    Adding new tasks

---
+++ L04. MergeTextFiles/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            var firstInputReader = new StreamReader(firstInputFilePath);
+            List<string> first = new List<string>();
+            List<string> second = new List<string>();
+
+            using (firstInputReader)
+            {
+                string firstLine;
+                while ((firstLine = firstInputReader.ReadLine()) != null)
+                {  
+                   first.Add(firstLine);
+                }
+
+                var secondInputReader = new StreamReader(secondInputFilePath);
+                
+                using (secondInputReader)
+                {
+                    string secondLine;
+                    while ((secondLine = secondInputReader.ReadLine()) != null)
+                    {
+                        second.Add(secondLine);
+                    }
+                }
+
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    if (first.Count > second.Count)
+                    {
+                        for (int i = 0; i < first.Count; i++)
+                        {
+                            writer.WriteLine(first[i]);
+
+                            if (i < second.Count)
+                            {
+                                writer.WriteLine(second[i]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < second.Count; i++)
+                        {
+                            if (i < first.Count)
+                            {
+                                writer.WriteLine(first[i]);
+                            }
+                            writer.WriteLine(second[i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ L05. ExtractSpecialBytes/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ExtractSpecialBytes
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            var sb = new StringBuilder();
+
+            using (var readerBit = new StreamReader(bytesFilePath))
+            {
+                while (!readerBit.EndOfStream)
+                {
+                    sb.Append(readerBit.ReadLine() + " ");
+                }
+            }
+
+            var bytes = sb.ToString().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(byte.Parse);
+
+            using (FileStream reader = new FileStream(binaryFilePath, FileMode.Open))
+            {
+
+                byte[] buff = new byte[reader.Length];
+                reader.Read(buff, 0, buff.Length);
+
+                using(FileStream write = new FileStream(outputPath, FileMode.OpenOrCreate))
+                {
+                    foreach (byte item in buff)
+                    {
+                        if (bytes.Contains(item))
+                        {
+                            write.WriteByte(item);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
